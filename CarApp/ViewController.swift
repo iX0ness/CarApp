a70cee1be6d50eb978350045fde3e486d2793435
Mykhaylo Levchuk <mykhaylo.levchuk@ukr.net>
2022-07-09 14:12:07 +0200

    initial commit

---
+++ CarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarApp
+//
+//  Created by Mykhaylo Levchuk on 09/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
